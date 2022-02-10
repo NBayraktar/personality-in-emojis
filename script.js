585461bb50619008c8f963d005535d2d910455b4
Nazim Bayraktar <nab1079@hotmail.com>
2022-02-10 10:51:14 +0000

    Little changes on index.html and style.css, also add some functions when button been pressed(script.js)

---
+++ script.js
@@ -0,0 +1,31 @@
+const myEmojis = [];
+// const getName = prompt('Add your name please!');
+const nameEl = document.getElementById('user-name');
+const inputEl = document.getElementById('input-el');
+const emojiEl = document.getElementById('emoji-box');
+const unshiftBtn = document.getElementById('btn-unshift');
+const pushBtn = document.getElementById('btn-push');
+const shiftBtn = document.getElementById('btn-shift');
+const popBtn = document.getElementById('btn-pop');
+
+// nameEl.innerHTML = `${getName} Emojis`;
+
+
+unshiftBtn.addEventListener('click', function () {
+
+  if (inputEl.value) {
+    myEmojis.unshift(inputEl.value);
+    renderEmojis()
+  }
+})
+
+
+function renderEmojis() {
+  emojiEl.innerHTML = '';
+  for (let i = 0; i < myEmojis.length; i++) {
+    const spanEl = document.createElement('span');
+    spanEl.innerHTML = myEmojis[i];
+    emojiEl.append(spanEl);
+    inputEl.value = '';
+  }
+}
